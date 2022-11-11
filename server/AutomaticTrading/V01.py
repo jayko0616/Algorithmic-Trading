22bfa7da8603acc55256770e1ef36d6e3d9aa184
na1-4an <parkne0114@naver.com>
2022-11-11 17:34:25 +0900

    221111_변동성돌파전략_코드수정

--- server/AutomaticTrading/V01.py
+++ server/AutomaticTrading/V01.py
@@ -1,14 +1,11 @@
-# 1초에 한번씩 현재가 가져오기
 import pyupbit
-import pprint
 
 f = open("upbit.txt")
 lines = f.readlines()
 access = lines[0].strip()
 secret = lines[1].strip()
 f.close()
-upbit = pybit.Ubit(access, secret)
 
-uuid = ""
-resp = upbit.cancel_order(uuid)
-pprint.pprint(resp)
+upbit = pyupbit.Upbit(access, secret)
+balance = upbit.get_balance("KRW")
+print(balance)

afea56da03e25c0f810accb3166cb37424f2efba
Jeong Dahyun <tv11111@naver.com>
2022-11-14 23:54:21 +0900

    menu item link!

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
-import { useNavigate } from 'react-router-dom';
 
 
 const MenuItem = ({active, children, to}) => (
-  <div className='menu-item'>{children}</div>
+  <Link to={to} className='menu-item'>{children}</Link>
 );
 
 const Header = () => {
 
-  const navigate = useNavigate();
-
-  const portfolio = ()=> {
-    navigate('/portfolio')
-    }
 
   return(
     <header className='header'>
@@ -23,8 +18,8 @@ const Header = () => {
         <div className='menu'>
           <Button className='btn_inputstock'>주식</Button>
           <Button className='btn_inputcoin'>코인</Button>
-          <MenuItem>홈</MenuItem>
-          <MenuItem onclick = {portfolio}>포트폴리오</MenuItem>
+          <MenuItem to={'/'}>홈</MenuItem>
+          <MenuItem to={'/portfolio'}>포트폴리오</MenuItem>
           <MenuItem>내정보</MenuItem>
           <MenuItem>공지</MenuItem>
         </div>

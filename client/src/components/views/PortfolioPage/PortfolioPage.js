aae3e5f33d23bb90b3f83f7bc889b3cbec3dbdfb
na1-4an <parkne0114@naver.com>
2022-11-15 00:31:41 +0900

    페이지 이동

---
+++ client/src/components/views/PortfolioPage/PortfolioPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+
+function PortfolioPage (){ 
+
+    return (
+        <div>
+            이건 포트폴리오 ~~
+        </div>
+  )
+}
+
+
+export default PortfolioPage;

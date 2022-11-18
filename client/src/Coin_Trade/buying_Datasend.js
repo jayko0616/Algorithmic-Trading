4d03d9b57351e2c0e9f11a6b8625db689bd065c2
User <wjdanr0869@naver.com>
2022-11-18 12:50:40 +0900

    2022_11_18_12:50

--- client/src/Coin_Trade/buying_Datasend.js
+++ client/src/Coin_Trade/buying_Datasend.js
@@ -1,10 +1,18 @@
 import axios from "axios";
+import coin_buy_data from "./coin_buy_data.json"
+const fs = require('fs')
 
 
 const buying_Datasend = (buying_price, quantity, buy_sum) =>{
     const client = axios.create();   // axios 기능생성
     const price = buying_price
     const quantity_ = quantity   
+    const buy_data = []
+    buy_data.push([price,quantity_]);
+    coin_buy_data.push(buy_data);
+    const coin_buy_data = JSON.stringify(buy_data)
+    // fs.writeFileSync("coin_buy_data.json",coin_buy_data);
+    coin_buy_data.push('asd')
     return client.post('/api/users/coin/buy' , {price, quantity_, buy_sum} );   //axios 기능을 통한 post 사용및 name 값 전달.
 }
 

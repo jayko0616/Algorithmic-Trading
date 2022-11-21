8df477f18dcbabc5d1bd878c8053c74c64e7e559
User <wjdanr0869@naver.com>
2022-11-21 12:16:59 +0900

    2022_11_21 12:16

--- client/src/Coin_Trade/selling_Datasend.js
+++ client/src/Coin_Trade/selling_Datasend.js
@@ -5,9 +5,6 @@ const selling_Datasend = (sell_price, quantity, sell_sum) =>{
     const client = axios.create();   // axios 기능생성
     const price = sell_price
     const quantity_ = quantity
-    const sell_data = []
-    sell_data.push([price,quantity_]);
-    const coin_sell_data = JSON.stringify(sell_data)
     // fs.writeFileSync("coin_sell_data.json",coin_sell_data);
     
     return client.post('/api/users/coin/sell' , {price, quantity_,sell_sum} );   //axios 기능을 통한 post 사용및 name 값 전달.
